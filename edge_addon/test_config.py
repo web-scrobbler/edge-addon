82d2711594e3bb2b29587e4ece87bbed1a078306
Malachi Soord <inverse.chi@gmail.com>
2025-07-06 00:48:22 +0200

    additional test

--- edge_addon/test_config.py
+++ edge_addon/test_config.py
@@ -38,6 +38,39 @@ class TestOptions(unittest.TestCase):
             self.assertEqual(options.retry_count, int(retry_count))
             self.assertEqual(options.sleep_seconds, int(sleep_seconds))
 
+    def test_create_options_debug_false(self):
+        script_name = "script_name"
+        product_id = "product_id"
+        client_id = "client_id"
+        api_key = "api_key"
+        file_path = "/path/to/addon.zip"
+        notes = "Release notes"
+        debug = "false"
+        retry_count = "3"
+        sleep_seconds = "5"
+
+        test_args = [
+            script_name,
+            product_id,
+            client_id,
+            api_key,
+            file_path,
+            notes,
+            debug,
+            retry_count,
+            sleep_seconds,
+        ]
+        with patch("sys.argv", test_args):
+            options = create_options()
+            self.assertEqual(options.product_id, product_id)
+            self.assertEqual(options.client_id, client_id)
+            self.assertEqual(options.api_key, api_key)
+            self.assertEqual(options.file_path, file_path)
+            self.assertEqual(options.notes, notes)
+            self.assertFalse(options.debug)
+            self.assertEqual(options.retry_count, int(retry_count))
+            self.assertEqual(options.sleep_seconds, int(sleep_seconds))
+
     def test_options_to_edge_options(self):
         script_name = "script_name"
         product_id = "product_id"
